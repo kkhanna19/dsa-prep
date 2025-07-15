a601125f0ff1ed52128630960077bf48689466fb
kkhanna19 <kajalkhanna271@gmail.com>
2025-07-15 21:32:26 +0530

    first commit

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -13,10 +13,12 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data){     //Sc = O(1);
         // step1 = create new node
         Node newNode = new Node(data);
+        size++;
 
         if (head == null){
            head = tail  = newNode;
@@ -32,6 +34,7 @@ public class LinkedList {
 
     public void addLast(int data){      //Sc = O(1)
         Node newNode = new Node(data);
+        size++;
         if(head == null){
             head = tail = newNode;
             return;
@@ -53,16 +56,131 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+      // i = idx -1; temp = prev
+       newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+           int val = head.data;
+           head = tail = null;
+           size = 0;
+           return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev : i = size -2
+        Node prev = head;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data; //tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
+    public int itrSearch(int key){    //O(n)
+        Node temp = head;
+        int i = 0;
+        while ( temp != null){
+            if (temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        // key not found
+        return -1;
+    }
+    public int helper(Node head, int key){    //actual recursive function   TC - O(n), SC - O(n)
+       if(head == null){
+           return -1;
+       }
+       if(head.data == key){
+           return 0;
+       }
+       int idx = helper(head.next, key);
+       if(idx == -1){
+           return -1;
+       }
+       return idx+1;
+    }
+    public int recursiveSearch(int key){
+    return helper(head, key);
+    }
+
+    public void reverse(){    //O(n)
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr= next;
+        }
+        head = prev;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addFirst(1);
-        ll.print();
-        ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+        ll.addLast(5);
+
+        ll.add(2, 3);
+        ll.print();
+//        System.out.println("size of the ll: " + ll.size);
+//        ll.removeFirst();
+//        ll.print();
+
+//        ll.removeLast();
+//        ll.print();
+//        System.out.println(ll.size);
+
+//        System.out.println(ll.itrSearch(3));
+//        System.out.println(ll.itrSearch(10));
+
+//        System.out.println(ll.recursiveSearch(3));
+//        System.out.println(ll.recursiveSearch(10));
+
+        ll.reverse();
         ll.print();
     }
 }
