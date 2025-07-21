00516def1c181670a601a4276e6eb103e3d4124d
kkhanna19 <kajalkhanna271@gmail.com>
2025-07-21 11:17:00 +0530

    first commit

---
+++ src/LinkedList/ZigZag.java
@@ -0,0 +1,88 @@
+package LinkedList;
+
+public class ZigZag {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+    
+    public void print(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void zigZag(){
+        // find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null){
+           next = curr.next;
+           curr.next = prev;
+           prev = curr;
+           curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextLeft, nextRight;
+
+        // zig-zag merge
+        while (left != null && right != null){
+            nextLeft = left.next;
+            left.next = right;
+            nextRight = right.next;
+            right.next = nextLeft;
+
+            left = nextLeft;
+            right = nextRight;
+        }
+    }
+
+    public static void main(String[] args) {
+        ZigZag ll = new ZigZag();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+        
+        ll.print();
+        ll.zigZag();
+        ll.print();
+
+    }
+}
