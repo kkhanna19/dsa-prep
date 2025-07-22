2e1dbaff7113a9ca0fdc91cbcfe20b34ab4970c7
kkhanna19 <kajalkhanna271@gmail.com>
2025-07-22 15:11:39 +0530

    first commit

---
+++ src/LinkedList/PalindromeLL.java
@@ -0,0 +1,96 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class PalindromeLL {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void  print(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public boolean checkPalindrome(){
+        if (head == null || head.next == null) return true;
+
+        //find mid
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        //reverse the 2nd half after mid
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // compare both halves
+        Node right = prev;
+        Node left = head;
+        while (right != null){
+            if (left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt(); // number of elements
+//
+//        for (int i = 0; i < n; i++) {
+//            int val = sc.nextInt();
+//            addLast(val);       // directly insert into linked list
+//            System.out.println(val + " ");
+//        }
+
+        PalindromeLL ll = new PalindromeLL();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(2);
+        ll.addLast(1);
+
+        ll.print();
+        System.out.println(ll.checkPalindrome());
+
+    }
+}
