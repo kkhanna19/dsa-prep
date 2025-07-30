db8f2edb138298b942bec96d55c0624596d2f56f
kkhanna19 <kajalkhanna271@gmail.com>
2025-07-30 21:57:08 +0530

    dsa preparation

--- src/Array/BinarySearch.java
+++ src/Array/BinarySearch.java
@@ -11,7 +11,7 @@ public class BinarySearch {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("enter key: ");
+        System.out.println("Enter key: ");
         int key = sc.nextInt();
 
         System.out.println(binarySearch(arr,key));
