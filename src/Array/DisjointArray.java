a320d5c4d668fb8625b9c8a399d26a69b9a1828c
kkhanna19 <kajalkhanna271@gmail.com>
2025-07-31 14:40:40 +0530

    dsa preparation

--- src/Array/DisjointArray.java
+++ src/Array/DisjointArray.java
@@ -3,7 +3,7 @@ package Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class DisjointArray {
+public class DisjointArray {         //no common element
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("size of array 1: ");
