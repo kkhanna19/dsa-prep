0d3d678f0bec811efd22403631661eed9552b212
kkhanna19 <kajalkhanna271@gmail.com>
2025-08-10 17:10:20 +0530

    first commit

---
+++ src/Stack/Stack.java
@@ -0,0 +1,48 @@
+package Stack;
+
+import java.util.ArrayList;
+public class Stack {
+        static class StackOperations{
+        static ArrayList<Integer> list =new ArrayList<>();
+
+        //isEmpty
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+
+        // push
+        public static void push(int data){
+            list.add(data);
+        }
+
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        StackOperations s = new StackOperations();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
