8dec40453dfef9e43657ed34cd417503a5206075
kkhanna19 <kajalkhanna271@gmail.com>
2025-08-17 13:15:33 +0530

    dsa preparation

---
+++ src/Stack/MaxAreaRectangle.java
@@ -0,0 +1,56 @@
+package Stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class MaxAreaRectangle {
+    public static void maxArea(int[] arr){
+    int maxArea = 0;
+    int[] nsr = new int[arr.length];
+    int[] nsl = new int[arr.length];
+
+    //Next smaller right
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length-1; i >=0 ; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+            if (s.isEmpty()){
+                nsr[i] = arr.length;
+            }
+            else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //Next smaller left
+        s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //Current area : width = j-i-1 = nsr[i] - nsl[i] -1
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] -1;
+            int currArea = height * width;
+            System.out.print(currArea + " ");
+            maxArea = Math.max(currArea, maxArea);
+        }
+        System.out.println();
+        System.out.println("max area of histogram: " + maxArea);
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 6, 2, 3};  //heights
+        maxArea(arr);
+    }
+}
