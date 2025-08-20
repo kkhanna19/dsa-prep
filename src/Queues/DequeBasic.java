bb3e40d64f283dd1e99cd3da2e0ba22269788da7
kkhanna19 <kajalkhanna271@gmail.com>
2025-08-20 12:57:26 +0530

    dsa preparation

---
+++ src/Queues/DequeBasic.java
@@ -0,0 +1,21 @@
+package Queues;
+
+import java.util.*;
+
+public class DequeBasic {
+    public static void main(String[] args) {
+        Deque <Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+//        deque.removeFirst();
+        deque.removeLast();
+        System.out.println(deque);
+
+        System.out.println("First element: " + deque.getFirst());
+        System.out.println("Last element: " + deque.getLast());
+
+    }
+}
