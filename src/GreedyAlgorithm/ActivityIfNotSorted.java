8e2d8a8e02fe8937f88cc8be88694a7fe4540334
kkhanna19 <kajalkhanna271@gmail.com>
2025-09-04 11:31:40 +0530

    dsa preparation

--- src/GreedyAlgorithm/ActivityIfNotSorted.java
+++ src/GreedyAlgorithm/ActivityIfNotSorted.java
@@ -2,10 +2,22 @@ package GreedyAlgorithm;
 
 import java.util.*;
 
-public class ActivitySelection {
+public class ActivityIfNotSorted {
     public static void main(String[] args) {
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[] = {2, 4, 6, 7, 9, 9};
+
+        //if not sorted --> store
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+
+        // then sort
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
 
         //end time basis sorted
         int maxAct = 0;
@@ -13,14 +25,14 @@ public class ActivitySelection {
 
         //1st activity
         maxAct = 1;
-        ans.add(0);
-        int endTime = end[0];
+        ans.add(activities[0][0]);
+        int endTime = activities[0][2];
         for (int i = 1; i < end.length; i++) {
-            if(start[i] >= endTime){
+            if(activities[i][1] >= endTime){
                 //activity select
                 maxAct++;
-                ans.add(i);
-                endTime = end[i];
+                ans.add(activities[i][0]);
+                endTime = activities[i][2];
             }
         }
         System.out.println(maxAct);
