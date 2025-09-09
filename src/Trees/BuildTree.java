78537981582cf35a88e0c0025d5f30db0c078819
kkhanna19 <kajalkhanna271@gmail.com>
2025-09-09 08:46:07 +0530

    dsa preparation

---
+++ src/Trees/BuildTree.java
@@ -0,0 +1,80 @@
+package Trees;
+
+public class BuildTree {    //o(n)
+     static class Node {
+         int data;
+         Node left;
+         Node right;
+
+         Node (int data){
+             this.data = data;
+             this.left = null;
+             this.right = null;
+         }
+     }
+
+     static class BinaryTree {
+         static int idx = -1;
+
+         public static Node buildTree(int nodes[]) {
+             idx++;
+             if (nodes[idx] == -1) {
+                 return null;
+             }
+             Node newNode = new Node(nodes[idx]);
+             newNode.left = buildTree(nodes);
+             newNode.right = buildTree(nodes);
+             return newNode;
+         }
+
+         public static void preorder(Node root) {      // # root-> left-> right
+             if (root == null) {
+                 System.out.print("-1 ");
+                 return;
+             }
+             System.out.print(root.data + " ");
+             preorder(root.left);
+             preorder(root.right);
+         }
+
+         public static void inorder(Node root){     //o(n)    #left->root->right
+             if(root == null){
+                 System.out.print("-1 ");
+                 return;
+             }
+             inorder(root.left);
+             System.out.print(root.data + " ");
+             inorder(root.right);
+         }
+
+         public static void postorder(Node root){
+             if(root == null){
+                 System.out.print("-1 ");
+                 return;
+             }
+             postorder(root.left);
+             postorder(root.right);
+             System.out.print(root.data + " ");
+         }
+     }
+
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+//        System.out.println(root.data);
+
+        System.out.print("preorder: ");
+        tree.preorder(root);
+
+        System.out.println();
+
+        System.out.print("Inorder: ");
+        tree.inorder(root);
+
+        System.out.println();
+
+        System.out.print("Postorder: ");
+        tree.postorder(root);
+    }
+}
