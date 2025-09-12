1f88f2ef00a29d402e545864bcafe38d1031c0f6
kkhanna19 <kajalkhanna271@gmail.com>
2025-09-12 09:30:47 +0530

    dsa preparation

--- src/Trees/TopViewOfTree.java
+++ src/Trees/TopViewOfTree.java
@@ -73,7 +73,7 @@ public class TopViewOfTree {
 
        */
 
-       Node root = new Node(1);
+        Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
