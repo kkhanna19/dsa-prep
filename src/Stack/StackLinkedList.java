62f5693f9e570395ebfbf4063b814c3432a108d2
kkhanna19 <kajalkhanna271@gmail.com>
2025-10-13 09:16:03 +0530

    first commit

--- src/Stack/StackLinkedList.java
+++ src/Stack/StackLinkedList.java
@@ -40,6 +40,13 @@ public class StackLinkedList {
                 return top;
             }
 
+
+
+
+
+
+
+
             //peek
             public static int peek() {
                 if (isEmpty()) {
